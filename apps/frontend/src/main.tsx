f0ef55f0b36e41792416c5fab14dabace27236eb
Dryss <drysscark@gmail.com>
2026-06-17 02:20:57 +0200

    setup Tailwind v4, design tokens et fonts

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
-import './styles/variables.css'
+import './lib/i18n'
+import './styles/tokens.css'
+import './styles/tailwind.css'
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
